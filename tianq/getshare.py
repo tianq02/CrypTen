0e297da3a9be2211e923e0baae7c7feb34819803
tianq <11007635+tianq02@user.noreply.gitee.com>
2026-03-10 11:37:41 +0800

    minor improvement

--- tianq/getshare.py
+++ tianq/getshare.py
@@ -4,9 +4,9 @@ import crypten.communicator as comm
 
 crypten.init()
 
+
 @mpc.run_multiprocess(world_size=3)
 def get_shares():
-
     # 2 steps are taken:
     # 1. all parties collaboratively generate PRZS (Pseudo-Random Zero Sharing).
     #    PRZS is designed such that sum of all shares equals zero.
@@ -18,18 +18,18 @@ def get_shares():
     x_enc: crypten.mpc.MPCTensor = crypten.cryptensor([1.0, 2.0, 3.0], ptype=crypten.mpc.arithmetic)
 
     rank = comm.get().get_rank()
-
     # print(f"\nrank: {rank}, share={x_enc.share}")
 
     # Return the raw underlying share back to the main process
-    return x_enc.share.numpy()
+    return rank, x_enc.share.numpy()
+
 
 # The decorator returns a list: [result_from_rank_0, result_from_rank_1]
 shares = get_shares()
 
 # Now you have them as normal variables in Jupyter!
-for i,s in enumerate(shares):
-    print(f"Share {i}:{s}")
+for r, s in shares:
+    print(f"Rank: {r}, Share: {s}")
 
 # You can even manually add them together to see the original fixed-point values (65536, 131072...)
-print("Sum & Scale:", sum(shares)/65536)
+print("Sum & Scale:", sum(s for r, s in shares) / 65536)
